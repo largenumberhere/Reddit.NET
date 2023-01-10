d37278b2ea88039bd8cc395c3ae58e52e31f5081
largenumberhere <largeNumbergoeshere@gmail.com>
2023-01-10 14:00:52 +1100

    pushed to my branch

---
+++ src/Reddit.NET/Things/Post/MediaMetadataItem.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reddit.Things
+{
+    public class MediaMetadataItem
+    {
+
+
+        [JsonProperty("o")]
+        public OriginalImage[] OriginalImages { get; set; }
+    }
+
+    public class OriginalImage
+    {
+        [JsonProperty("u")]
+        public String URL { get; set; }
+    }
+}
